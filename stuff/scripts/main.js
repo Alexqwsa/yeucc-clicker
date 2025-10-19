636944b5b70db80362e3f089c7fec4039384cdf5
Alexqwsa <134790082+Alexqwsa@users.noreply.github.com>
2025-10-19 14:15:41 +0200

    made the button clickable now and some other stuff

---
+++ stuff/scripts/main.js
@@ -0,0 +1,9 @@
+yeucclings = 0;
+clickmulti = 1;
+
+
+// how do i add a yeuccling when the yeucc... button is clicked
+function addyeuccling() {
+    yeucclings += 1;
+    document.getElementById("yeuccling-bank-amount").innerText = yeucclings;
+}
