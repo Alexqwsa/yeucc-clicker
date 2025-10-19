a76e0792651590682ea12b19b7155f13ef44a98c
Alexqwsa <134790082+Alexqwsa@users.noreply.github.com>
2025-10-19 16:14:36 +0200

    hmmmmmmmmmm

--- stuff/scripts/main.js
+++ stuff/scripts/main.js
@@ -1,9 +1,26 @@
 yeucclings = 0;
 clickmulti = 1;
+clickmultiprice = 50;
 
-
-// how do i add a yeuccling when the yeucc... button is clicked
+// how do i add a yeuccling when the yeucc button is clicked
 function addyeuccling() {
-    yeucclings += 1;
+    yeucclings += clickmulti;
     document.getElementById("yeuccling-bank-amount").innerText = yeucclings;
+}
+
+// add multiplier
+function buyclickmultiplier() {
+    if (yeucclings >= clickmultiprice) {
+      yeucclings -= clickmultiprice;
+      clickmulti += 1;
+      clickmultiprice += 50;
+      document.getElementById("yeuccling-bank-amount").innerText = yeucclings;
+      document.getElementById("clickmultidisplay").innerText = "click multiplier: " + clickmulti;
+      document.getElementById("clickmulticostdisplay").innerText = "cost: " + clickmultiprice + " yeucclings";
+    }
+}
+//change the owner text on the credits when hovered on it
+function changeOwnerTextOnHover() {
+    let ownerText = document.querySelector("owner-text");
+    ownerText.style.color = "red";
 }
